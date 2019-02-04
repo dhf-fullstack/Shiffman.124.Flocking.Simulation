26fe9527a6a9a9c7b400b7cf0f3d36bef5b0bc5a
David H. Friedman <david.h.friedman.nyc@gmail.com>
2019-02-04 06:10:44 -0500

    flock

--- index.js
+++ index.js
@@ -6,39 +6,52 @@ function Canvas(w, h) {
   this.c.style.height = this.c.height = h;
   this.ctx = this.c.getContext('2d');
   //this.ctx.globalCompositeOperation = 'destination-over';
+
+  this.boids = [];
+  for(let i = 0; i < 30; i++) {
+    this.boids.push(new Boid(this.w, this.h))
+  }
+
+  this.start = function() {
+    window.requestAnimationFrame(this.draw.bind(this));
+  }
+
+  this.draw = function (ts) {
+    //console.log('drawing', ts, 'fps', 1000/(ts-this.prev_ts));
+    //this.prev_ts = ts;
+
+    this.ctx.clearRect(0, 0, this.w, this.h);
+    this.boids.forEach(b => {
+      b.draw(this.ctx);
+      b.update();
+    })
+    window.requestAnimationFrame(this.draw.bind(this));
+  }
 }
 
-const c = new Canvas(320, 200)
+function Boid(w, h) {
+  this.w = w;
+  this.h = h;
+  this.x = Math.floor(Math.random()*w);
+  this.y = Math.floor(Math.random()*h);
+  this.vx = 10; //(Math.random()*10)-5;
+  this.vy = 10; //(Math.random()*10)-5;
 
-const init = (function() {
-  console.log('init');
-  this.guy = {};
-  this.guy.x = 0;
-  this.guy.y = 0;
-  this.guy.dx = 1;
-  this.guy.dy = 2;
-  window.requestAnimationFrame(draw);
-}).bind(c);
-
-const draw = (function (ts) {
-  //console.log('drawing', ts,
-  //'fps', 1000/(ts-this.prev_ts));
-  //this.prev_ts = ts;
-  this.ctx.clearRect(0, 0, this.w, this.h);
-  //console.log("CLEAR", this.w, this.h);
-  //this.ctx.fillStyle = 'rgba(0, 0, 0, 0.4)';
-  //this.ctx.strokeStyle = 'rgba(0, 153, 255, 0.4)';
-  this.ctx.strokeRect(this.guy.x, this.guy.y, 10, 10);
-  //this.ctx.stroke();
-  this.guy.x += this.guy.dx;
-  this.guy.y += this.guy.dy;
-  if (this.guy.x > this.w || this.guy.x < 0) {
-    this.guy.dx = -this.guy.dx;
+  this.draw = function(ctx) {
+    ctx.strokeRect(this.x, this.y, 10, 10);
   }
-  if (this.guy.y > this.h || this.guy.y < 0) {
-    this.guy.dy = -this.guy.dy;
-  }
-  window.requestAnimationFrame(draw);
-}).bind(c);
 
-init();
+  this.update = function() {
+    this.x += this.vx;
+    this.y += this.vy;
+    if (this.x > this.w || this.x < 0) {
+      this.vx = -this.vx;
+    }
+    if (this.y > this.h || this.y < 0) {
+      this.vy = -this.vy;
+    }
+  }
+}
+
+const c = new Canvas(320, 240);
+c.start();
