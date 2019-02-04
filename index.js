dd80b63ec516b3b5b481b150640303043bd57d3b
David H. Friedman <david.h.friedman.nyc@gmail.com>
2019-02-04 18:59:18 -0500

    begininng flocking behavior

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ function Canvas(w, h) {
   this.c.style.width =  this.c.width = w;
   this.c.style.height = this.c.height = h;
   this.ctx = this.c.getContext('2d');
+  this.run = false;
+  document.getElementById('runButton').innerText = 'Start';
   //this.ctx.globalCompositeOperation = 'destination-over';
 
   this.boids = [];
   for(let i = 0; i < 30; i++) {
-    this.boids.push(new Boid(this.w, this.h))
+    this.boids.push(new Boid(this.w, this.h, this.boids))
   }
 
   this.start = function() {
@@ -18,15 +20,17 @@ function Canvas(w, h) {
   }
 
   this.draw = function (ts) {
-    //console.log('drawing', ts, 'fps', 1000/(ts-this.prev_ts));
-    //this.prev_ts = ts;
+    console.log('drawing', ts, 'fps', 1000/(ts-this.prev_ts));
+    this.prev_ts = ts;
 
     this.ctx.clearRect(0, 0, this.w, this.h);
     this.boids.forEach(b => {
       b.draw(this.ctx);
       b.update();
     })
-    window.requestAnimationFrame(this.draw.bind(this));
+    if (this.run) {
+      window.requestAnimationFrame(this.draw.bind(this));
+    }
   }
 }
 
@@ -35,19 +39,25 @@ function rand_velocity(v_min, v_max) {
          * (Math.random()*(v_max-v_min)+v_min)
 }
 
-function Boid(w, h) {
+function normalize_vector(x, y) {
+  let m = Math.sqrt(x*x + y*y);
+  return [x/m, y/m];
+}
+
+function Boid(w, h, flock) {
   this.w = w;
   this.h = h;
+  this.flock = flock; // ref to array
   this.x = Math.floor(Math.random()*w);
   this.y = Math.floor(Math.random()*h);
-  const v_min = 1;
-  const v_max = 5;
-  this.vx = rand_velocity(v_min, v_max)
-  this.vy = rand_velocity(v_min, v_max)
+  const v_min = 4;
+  const v_max = 7;
+  this.vx = rand_velocity(v_min, v_max);
+  this.vy = rand_velocity(v_min, v_max);
   this.path = new Path2D();
-  this.path.moveTo(-3, -5)
-  this.path.lineTo(3, -5)
-  this.path.lineTo(0, 10)
+  this.path.moveTo(-3, -5);
+  this.path.lineTo(3, -5);
+  this.path.lineTo(0, 10);
   this.path.closePath();
 
   this.draw = function(ctx) {
@@ -64,13 +74,35 @@ function Boid(w, h) {
     ctx.save()
     ctx.translate(this.x, this.y)
     ctx.rotate(angle);
-    //ctx.translate(this.x, this.y)
     ctx.stroke(this.path);
-    //ctx.strokeRect(this.x, this.y, 10, 10);
     ctx.restore();
   }
 
+  this.distance_from = function (x, y) {
+    return Math.sqrt((this.x - x)*(this.x - x) + (this.y - y)*(this.y - y))
+  }
+
   this.update = function() {
+    // separation, alignment, cohesion (https://www.red3d.com/cwr/boids/)
+
+    // local flockmates are within a certain distance of boid center and within an angle of boid direction.
+    // start with a naive O(n^2) approach comparing every pair of boids
+    const local = this.flock.filter(b => this.distance_from(b.x, b.y) < 50)
+    console.log('flock: ', local.length)
+
+    // avoid crowding local flockmates
+
+    // steer toward the average heading of local flockmates
+    local.forEach((b,i) => {
+      console.log(`${i}: ${[b.vx.toFixed(2), b.vy.toFixed(2)]}`);
+    })
+
+    let avg_x = local.reduce((x, b) => x + b.x, 0.0);
+    let avg_y = local.reduce((y, b) => y + b.y, 0.0);
+    [avg_x, avg_y] = normalize_vector(avg_x, avg_y);
+    console.log(`Average heading: ${[avg_x.toFixed(2), avg_y.toFixed(2)]}`);
+    // steer toward the average position of local flockmates
+
     this.x += this.vx;
     this.y += this.vy;
     if (this.x > this.w || this.x < 0) {
@@ -83,4 +115,16 @@ function Boid(w, h) {
 }
 
 const c = new Canvas(320, 240);
+
+document.getElementById('runButton').addEventListener('click', e => {
+  c.run = !c.run;
+  e.target.innerText = c.run ? "Stop" : "Start";
+  if (c.run) { c.start() }
+})
+
+document.getElementById('stepButton').addEventListener('click', e => {
+  c.run = false;
+  c.start();
+})
+
 c.start();
